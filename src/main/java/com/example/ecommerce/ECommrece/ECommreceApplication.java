35fdf564339343881965abc5e80ada64f97ea3d5
bogaledemasrepo <bogidemas@gmail.com>
2026-07-27 23:05:27 +0300

    Initial commit for ECommerce project

---
+++ src/main/java/com/example/ecommerce/ECommrece/ECommreceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.ECommrece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommreceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommreceApplication.class, args);
+	}
+
+}
