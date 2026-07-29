bd22e3769d29188dff53b828cd4c1a8c86b1acbc
bogaledemasrepo <bogidemas@gmail.com>
2026-07-29 10:45:44 +0300

    Issue fix and deployment setup completed

--- src/main/java/com/ecommerce/config/JwtService.java
+++ src/main/java/com/ecommerce/config/JwtService.java
@@ -17,10 +17,10 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    @Value("${jwt.secret}")
+    @Value("${application.security.jwt.secret}")
     private String secretKey;
 
-    @Value("${jwt.expiration}")
+    @Value("${application.security.jwt.expiration}")
     private long jwtExpiration;
 
     public String extractUsername(String token) {
